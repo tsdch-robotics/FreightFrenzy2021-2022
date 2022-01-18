62efb1d8aeff3e394b4a5db4e0e58f8151bc5d11
Bernardo <22bernardoa@tsdch.org>
2022-01-18 10:36:04 -0500

    Update PIDController.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFiles/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFiles/PIDController.java
@@ -4,17 +4,13 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.SerialNumber;
 
 import org.firstinspires.ftc.teamcode.Hardware.ChampBot_v2;
 
-public class PIDController extends LinearOpMode {
+public abstract class PIDController extends LinearOpMode {
 
-    public DcMotor DriveFrontLeft;
-    public DcMotor DriveFrontRight;
-    public DcMotor DriveBackLeft;
-    public DcMotor DriveBackRight;
-
-    private BNO055IMU imu; //model number of inertial measurment unit in REV Expansion hub
+    private BNO055IMU imu; //model number of inertial measurement unit in REV Expansion hub
 
     double integralSum = 0;
     double Kp = 0;
@@ -30,18 +26,33 @@ public class PIDController extends LinearOpMode {
         ChampBot_v2 robot = new ChampBot_v2();
         robot.init(hardwareMap);
 
-        DriveFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        DriveFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        DriveBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        DriveBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        //imu = hardwareMap.get(BNO055IMU.class, imu);
+        imu = hardwareMap.get(BNO055IMU.class, (SerialNumber) imu); // added (SerialNumber) as suggested by debugger
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
 
-        //waitforStart();
+        waitForStart();
+
+        double referenceAngle = Math.toRadians(90);
 
         while (opModeIsActive()) {
-            double power = PIDControl(100, DriveFrontLeft.getCurrentPosition());
-            DriveFrontLeft.setPower(power);
+            double power = PIDControl(100, robot.DriveFrontLeft.getCurrentPosition()); //This references one motor, can use multiple as reference if calculate powers for separate motors
+            robot.DriveFrontLeft.setPower(power);
+            robot.DriveFrontRight.setPower(power);
+            robot.DriveBackLeft.setPower(power);
+            robot.DriveBackRight.setPower(power);
+
+            power = PIDControl(referenceAngle, imu.getAngularOrientation().firstAngle); //Angle Control
+            robot.DriveFrontLeft.setPower(power);
+            robot.DriveFrontRight.setPower(power);
+            robot.DriveBackLeft.setPower(power);
+            robot.DriveBackRight.setPower(power);
         }
     }
 
@@ -55,5 +66,15 @@ public class PIDController extends LinearOpMode {
 
         double output = (error * Kp) + (derivative * Kd) + (integralSum * Ki);
         return output;
+    }
+
+    public double angleWrap(double radians) {
+        while (radians > Math.PI) {
+            radians -= 2 * Math.PI;
+        }
+        while (radians < Math.PI) {
+            radians += 2 * Math.PI;
+        }
+        return radians;
     }
 }
