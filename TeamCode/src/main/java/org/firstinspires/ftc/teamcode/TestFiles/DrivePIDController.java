9efcdf58ff8e5e399a51c58c960304c125aa63b7
Bernardo <22bernardoa@tsdch.org>
2022-01-26 23:04:46 -0500

    Create DrivePIDController.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFiles/DrivePIDController.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.TestFiles;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class DrivePIDController {
+    private double targetEncoder;
+    private double kP, kI, kD;
+    private double accumulatedError = 0;
+    private ElapsedTime timer = new ElapsedTime();
+    private double lastError = 0;
+    private double lastTime = 0;
+    public DrivePIDController(double target, double p, double i, double d) {
+        targetEncoder = target;
+        kP = p;
+        kI = i;
+        kD = d;
+    }
+    public double update(double currentPosition) {
+        // P
+        double error = targetEncoder - currentPosition;
+
+        // I
+        accumulatedError += error;
+        if (Math.abs(error) < 1) {
+            accumulatedError = 0;
+        }
+        accumulatedError = Math.abs(accumulatedError) * Math.signum(error);
+
+        // D
+        double slope = 0;
+        if (lastTime > 0) {
+            slope = (error - lastError)/ (timer.milliseconds() - lastTime);
+        }
+        lastTime = timer.milliseconds();
+        lastError = error;
+
+        //motor power calculations
+        double motorPower = 0.1 * Math.signum(error) + 0.9 * Math.tanh(kP * error + kI * accumulatedError + kD * slope);
+        return motorPower;
+    }
+}
