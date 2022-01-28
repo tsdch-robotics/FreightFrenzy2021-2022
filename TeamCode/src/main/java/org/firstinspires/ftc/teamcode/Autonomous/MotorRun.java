1605d5052d62edf44f8a61cbbd1023a2aae1b53a
TSDCH Robotics (LabPC-5459) <trinityrobotics@tsdch.org>
2022-01-28 10:55:28 -0500

    Update MotorRun.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MotorRun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MotorRun.java
@@ -13,13 +13,25 @@ import java.lang.*;
 
 public class MotorRun extends LinearOpMode {
     ChampBot_v2 robot = new ChampBot_v2();
+    private ElapsedTime runtime = new ElapsedTime();
+    public DcMotor DriveFrontLeft; //:D
+    public DcMotor DriveFrontRight;
+    public DcMotor DriveBackLeft;
+    public DcMotor DriveBackRight;
 
     @Override
     public void runOpMode() {
         robot.init(hardwareMap);
         waitForStart();
 
+        robot.DriveFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.DriveBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         robot.DriveBackRight.setPower(0.5);
+        robot.DriveBackLeft.setPower(0.5);
+        robot.DriveFrontRight.setPower(0.5);
         robot.DriveFrontLeft.setPower(0.5);
 
     }
